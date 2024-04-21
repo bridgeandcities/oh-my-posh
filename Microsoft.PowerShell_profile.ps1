40809446d44e4cf48d9f2a5ed706e2cc397af6e5
bridgeandcities <dakaizhou.gz@gmail.com>
2024-04-22 11:41:30 +1200

    Update $profile with correct path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # for PowerShell (macOS)
 $env:POSH_THEMES_PATH = Join-Path -Path "$(brew --prefix oh-my-posh)" -ChildPath themes
 [scriptblock]::Create($(oh-my-posh completion powershell)).Invoke()
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/greg.omp.json" | iex
+oh-my-posh init pwsh --config "~/.config/oh-my-posh/greg.omp.json" | iex
 $env:POSH_AZURE_ENABLED = $true
